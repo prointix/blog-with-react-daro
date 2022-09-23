d3f90cda11dea3d0b175e37df797a8682a35ffcc
Daro-Sim <daroandrot@gmail.com>
2022-09-23 11:56:10 +0700

    Home fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import PostsDraft from "../components/PostsDraft";
 import { PostsPublic } from "../components/PostsPublic";
+import { useAuth } from "../contexts/auth";
 
 interface HomeProps {}
 
 export const Home: React.FC = () => {
+  const { state } = useAuth();
   const [active, setActive] = React.useState("Public Posts");
+  const navigate = useNavigate();
+
+  const loginNavigate = () => {
+    navigate("/signin");
+  };
   return (
     <>
       <nav className="container">
@@ -15,6 +23,11 @@ export const Home: React.FC = () => {
           Public Posts{" "}
         </button>
         <button onClick={() => setActive("Draft Posts")}> Draft Posts </button>
+        {state.signed ? (
+          <button>Logout</button>
+        ) : (
+          <button onClick={loginNavigate}>Login</button>
+        )}
       </nav>
       <div>{active === "Public Posts" ? <PostsPublic /> : <PostsDraft />}</div>
     </>
